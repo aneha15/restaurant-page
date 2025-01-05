24ff2a0f5238b39135e6f0a2e6b14aa1f0ea1364
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-05 20:22:38 +0600

    Add image to home page

--- src/home.js
+++ src/home.js
@@ -1,3 +1,4 @@
+import homepic from './home.jpg';
 
 const homepage = function () {
     const content = document.querySelector('#content');
@@ -5,8 +6,12 @@ const homepage = function () {
     home.id = 'home';
     home.innerHTML = `<h1>Welcome to Rise n Shine Bakery</h1>
        <p class="intro">Celebrate life's sweetest moments with 
-       our handcrafted bakes made with the finest quality 
-       ingredients.</p>`;
+       our handcrafted bakes.</p>`;
+    const image = document.createElement('img');
+    image.id = 'homepic'
+    image.src = homepic;
+    image.alt = 'Our bakery items';
+    home.appendChild(image);
     content.appendChild(home);
 };
 
