7b257a02a686d5bd449a4aec66c6837130dcbf16
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-05 20:58:56 +0600

    Allow tab browsing

--- src/pageload.js
+++
@@ -1,9 +0,0 @@
-import homepage from './home.js';
-import menupage from './menu.js';
-
-const load = function () {
-   // homepage();
-   menupage();
-};
-
-export default load;
--- src/script.js
+++ src/script.js
@@ -1,5 +1,32 @@
 import './style.css';
 
-import load from './pageload';
+import homepage from './home.js';
+import menupage from './menu.js';
+import aboutpage from './about.js';
+// import load from './pageload';
 
-load();
+const tabSwitch = function () {
+    const content = document.querySelector('#content');
+    const home = document.querySelector('.home');
+    const menu = document.querySelector('.menu');
+    const about = document.querySelector('.about');
+
+    homepage();
+
+    home.addEventListener('click', () => {
+        content.textContent = '';
+        homepage();
+    });
+    menu.addEventListener('click', () => {
+        content.textContent = '';
+        menupage();
+    });
+
+    about.addEventListener('click', () => {
+        content.textContent = '';
+        aboutpage();
+    });
+};
+
+
+tabSwitch();
