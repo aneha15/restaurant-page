d2c83b8b481639f355eb85fe5a8ddbe7374f4037
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-05 19:29:54 +0600

    Change home content

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ const homepage = function () {
     const content = document.querySelector('#content');
     const home = document.createElement('div');
     home.id = 'home';
-    home.innerHTML = `<h1>Welcome to Rose Bakery</h1>
+    home.innerHTML = `<h1>Welcome to Rise n Shine Bakery</h1>
        <p class="intro">Celebrate life's sweetest moments with 
        our handcrafted bakes made with the finest quality 
        ingredients.</p>`;
